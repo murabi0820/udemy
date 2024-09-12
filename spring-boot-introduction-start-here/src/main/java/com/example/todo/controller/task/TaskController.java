c65ea6164f497ce2f565b9017249912087ab90d2
murabi0820 <yuyuenoeno0820@gmail.com>
2024-09-12 21:58:41 +0900

    Add files via upload

---
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -0,0 +1,27 @@
+package com.example.todo.controller.task;
+
+import com.example.todo.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService ;
+
+    @GetMapping("/tasks")
+    public  String list(Model model) {
+        var taskList = taskService.find()
+                        .stream()
+                        .map(TaskDTO::toDTO)
+                        .toList();
+        model.addAttribute("taskList",taskList); //TODO
+        return "tasks/list";
+    }
+}
+
+
+
---
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/service/task/TaskService.java
@@ -0,0 +1,26 @@
+package com.example.todo.service.task;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+
+    public List<TaskEntity> find() {
+        var task1 = new TaskEntity(
+                1L,
+                "Spring Bootを学ぶ",
+                "TODO アプリケーションを学ぶ",
+                TaskStatus.TODO
+        );
+        var task2 = new TaskEntity(
+                2L,
+                "Spring Securityを学ぶ",
+                "ログイン機能を作ってみる",
+                TaskStatus.DOING
+        );
+        return List.of(task1,task2);
+    }
+}
